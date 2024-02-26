d1f80ac468631f7e19b69ea590cad8c159f419e7
Thomas Zierer <thomas.zierer@muenchen-mail.de>
2024-02-26 09:41:34 +0100

    Move getXinfoFromEvent() method

--- sonar-xinfo-plugin/src/main/java/de/tgmz/sonar/plugins/xinfo/AbstractXinfoProvider.java
+++ sonar-xinfo-plugin/src/main/java/de/tgmz/sonar/plugins/xinfo/AbstractXinfoProvider.java
@@ -19,6 +19,7 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 
@@ -31,6 +32,8 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import de.tgmz.sonar.plugins.xinfo.config.XinfoConfig;
+import de.tgmz.sonar.plugins.xinfo.generated.plicomp.FILE;
+import de.tgmz.sonar.plugins.xinfo.generated.plicomp.MESSAGE;
 import de.tgmz.sonar.plugins.xinfo.generated.plicomp.PACKAGE;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
@@ -63,7 +66,7 @@ public abstract class AbstractXinfoProvider implements IXinfoProvider {
 		this.configuration = configuration;
 	}
 
-	public PACKAGE createXinfo(InputStream is) throws XinfoException {
+	protected PACKAGE createXinfo(InputStream is) throws XinfoException {
 		PACKAGE p = null;
 		
 		// Perhaps we need to read the InputStream more than once so we save it to a ByteArray first
@@ -139,6 +142,55 @@ public abstract class AbstractXinfoProvider implements IXinfoProvider {
 		}
 		
 		return p;
+	}
+	protected PACKAGE getXinfoFromEvent(InputStream is) throws IOException {
+		de.tgmz.sonar.plugins.xinfo.generated.plicomp.ObjectFactory of = new de.tgmz.sonar.plugins.xinfo.generated.plicomp.ObjectFactory();
+		
+		PACKAGE ccomp = of.createPACKAGE();
+		ccomp.setFILEREFERENCETABLE(of.createFILEREFERENCETABLE());
+
+		List<String> readLines = IOUtils.readLines(is, Charset.defaultCharset());
+		
+		readLines.forEach(line -> {
+			// See https://www-40.ibm.com/servers/resourcelink/svc00100.nsf/pages/zOSV2R3sc147307/$file/cbcux01_v2r3.pdf
+			// page 648 for the detailed format of the SYSEVENT file
+			if (line.startsWith("ERROR")) {
+				// The ERROR field looks like this:
+				// ERROR 0 1 0 0 3 3 0 0 CCNnnnn E 12 26 Undeclared identifier add.
+				//       | | | | | | | | |       | |  |  |
+				//       A B C D E F G H I       J K  L  M
+				String[] s = line.split("\\s", 14);
+				
+				MESSAGE m = of.createMESSAGE();
+
+				m.setMSGFILE(s[2]);						// B: Increments starting with 1 for the primary file
+				m.setMSGLINE(s[5]);						// E: The source line number for which the message was issued.
+				m.setMSGNUMBER(s[9] + s[10]);			// I: String Containing the message identifier
+														// J: Message severity character (I/W/E/S/U) (processed in AbstractXinfoIssuesLoader)
+				m.setMSGTEXT(s[13]);					// M: String containing message text
+				
+				ccomp.getMESSAGE().add(m);
+			}
+			if (line.startsWith("FILEID")) {
+				// The FILEID field looks like this:
+				// FILEID 0 1 0 10 ./simple.c
+				//        | | | |  |
+				//        A B C D  E
+				String[] s = line.split("\\s", 6);
+				
+				FILE f = of.createFILE();
+				f.setFILENUMBER(s[2]);					// B: Increments starting with 1 for the primary file
+				f.setINCLUDEDONLINE(s[3]);				// C: The line number of the #include directive. For the primary source file this value is 0
+				f.setFILENAME(s[5])	;					// E: String containing file/dataset name.
+				
+				ccomp.getFILEREFERENCETABLE().getFILE().add(f);
+			}
+		});
+		
+		ccomp.getFILEREFERENCETABLE().setFILECOUNT(String.valueOf(ccomp.getFILEREFERENCETABLE().getFILE().size()));
+		
+		return ccomp;
+		
 	}
 
 	protected Configuration getConfiguration() {
