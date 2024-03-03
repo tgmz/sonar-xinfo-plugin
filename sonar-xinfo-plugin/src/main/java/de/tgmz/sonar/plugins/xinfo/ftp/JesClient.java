a4e2aa8a712476d9571e04fcba18398745f44f55
Thomas Zierer <thomas.zierer@muenchen-mail.de>
2024-03-03 18:54:17 +0100

    JES utility classes

---
+++ sonar-xinfo-plugin/src/main/java/de/tgmz/sonar/plugins/xinfo/ftp/JesClient.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+  * Copyright (c) 26.02.2024 Thomas Zierer.
+  * All rights reserved. This program and the accompanying materials
+  * are made available under the terms of the Eclipse Public License v2.0
+  * which accompanies this distribution, and is available at
+  * http://www.eclipse.org/legal/epl-v20.html
+  *
+  * Contributors:
+  *    Thomas Zierer - initial API and implementation and/or initial documentation
+  *******************************************************************************/
+package de.tgmz.sonar.plugins.xinfo.ftp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+public class JesClient extends FTPClient {
+    public int setOwnerFilter(String owner) throws IOException {
+        return site(String.format("JesOWNER=%s", owner));
+    }
+
+    public List<JesJob> listJobsDetailed() throws IOException {
+        List<JesJob> jobs = new ArrayList<>();
+        for (FTPFile file : listFiles()) {
+            JesJob job = new JesJob();
+            if (job.parseDetails(file.getRawListing())) {
+                jobs.add(job);
+            }
+        }
+        return jobs;
+    }
+
+    public JesJob submit(String sourceJCL) throws IOException {
+        OutputStream outputStream = storeFileStream("job");
+        outputStream.write(sourceJCL.getBytes());
+        outputStream.close();
+        completePendingCommand();
+
+        Pattern pattern = Pattern.compile("It is known to J[Ee][Ss] as (?<handle>.+)");
+        Matcher matcher = pattern.matcher(getReplyString());
+        JesJob job = null;
+        if (matcher.find()) {
+            job = new JesJob();
+            job.setHandle(matcher.group("handle"));
+        }
+        return job;
+    }
+}
